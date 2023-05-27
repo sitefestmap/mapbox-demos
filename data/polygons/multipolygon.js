269c040d6611542e7b12173546e6e7ab788a7176
mattweston <68040892+mattweston@users.noreply.github.com>
2023-05-27 12:34:15 +0100

    multipolygon

---
+++ data/polygons/multipolygon.js
@@ -0,0 +1,129 @@
+
+const center_bottom = [-2.217850418218603, 51.74317023834362];
+const center_bottom_left = [-2.223084174810481, 51.745370277385945];
+const center_top_left = [-2.220203207878683, 51.74711993736873];
+const center_top_right = [-2.214924597164244, 51.74534944307196];
+const center_bottom_right = [-2.214825402940761, 51.74337835473324];
+
+const outside_bottom = [-2.206785, 51.680108];
+const outside_bottom_left = [-2.323886, 51.727967];
+const outside_bottom_right = [-2.12433, 51.723189];
+const outside_top_left = [-2.247626, 51.794135];
+const outside_top_right = [-2.12014, 51.784184];
+
+//const polygon_colors = ['#f84d4d', '#4264fb', '#40546a', '#f79640', '#7753eb', '#207a4b'];
+
+const red = '#f84d4d'
+const mid_blue = '#4264fb'
+const blue = '#40546a' 
+const orange = '#f79640'
+const purple = '#7753eb'
+const green = '#207a4b'
+
+const multi_polygon = {
+    type: 'FeatureCollection',
+    features: [
+        {
+            type: 'Feature',
+            properties: { color: red},
+            geometry: {
+                type: 'Polygon',
+                coordinates: [ // 1 Town Center
+                    [
+                        center_bottom,
+                        center_bottom_left,
+                        center_top_left,
+                        center_top_right,
+                        center_bottom_right,
+                        center_bottom,
+                    ]
+                ]
+            }
+        },
+        {   // 2 Nailsworth
+            type: 'Feature',
+            properties: { color: mid_blue},
+            geometry: {
+                type: 'Polygon',
+                coordinates: [
+                    [
+                        center_bottom,
+                        center_bottom_left,
+                        outside_bottom_left,
+                        outside_bottom,
+                        center_bottom
+                    ]
+                ]
+            }
+        },
+        {  // 3 Vale
+            type: 'Feature',
+            properties: { color: blue},
+            geometry: {
+                type: 'Polygon',
+                coordinates: [
+                    [
+                        center_top_left,
+                        center_bottom_left,
+                        outside_bottom_left,
+                        outside_top_left,
+                        center_top_left
+                    ]
+                ]
+            }
+        },
+        {
+            // 4 Slad
+            type: 'Feature',
+            properties: { color: orange },
+            geometry: {
+                type: 'Polygon',
+                coordinates: [
+                    [
+                        center_top_left,
+                        outside_top_left,
+                        outside_top_right,
+                        center_top_right,
+                        center_top_left,
+                    ]
+                ]
+            }
+        },
+        {
+            // 5 Bisley
+            type: 'Feature',
+            properties: { color: purple },
+            geometry: {
+                type: 'Polygon',
+                coordinates: [
+                    [
+                        center_top_right,
+                        outside_top_right,
+                        outside_bottom_right,
+                        center_bottom_right,
+                        center_top_right
+                    ]
+                ]
+            }
+        },
+        {
+            // 6 Golden Valley
+            type: 'Feature',
+            properties: { color: green},
+            geometry: {
+                type: 'Polygon',
+                coordinates: [
+                    [
+                        center_bottom_right,
+                        center_bottom,
+                        outside_bottom,
+                        outside_bottom_right,
+                        center_bottom_right
+                    ]
+                ]
+            }
+        }
+    ]
+};
+
+export default multi_polygon;
--- scripts/map.js
+++ scripts/map.js
@@ -4,6 +4,7 @@
     * @markers : bath rd / bath rd 135 icons not showing
     * @waypoints : get waypoints from added layerIDs
     * @popups : add artist info for each studio
+    * @checkboxes : slides are checking all artists / need to change to studios instead?
  */
 
 import mapboxgl from 'mapbox-gl';
@@ -12,12 +13,24 @@ import mapboxgl from 'mapbox-gl';
 // import MapboxDirections from 'mapbox/mapbox-gl-directions';
 
 // import polygon from '/data/polygons/pentagon.json' assert { type: 'json' };
-import polygon from '/data/polygons/triangle.json' assert { type: 'json' };
+// import polygon from '/data/polygons/triangle.json' assert { type: 'json' };
 // import polygon from '/data/polygons/square.json' assert { type: 'json' };
 // import polygon from '/data/polygons/circle.json' assert { type: 'json' };
 
+// Polygons
+import towncentre from '/data/polygons/towncentre.json' assert { type: 'json' };
+import nailsworth from '/data/polygons/nailsworth.json' assert { type: 'json' };
+import vale from '/data/polygons/vale.json' assert { type: 'json' };
+import slad from '/data/polygons/slad.json' assert { type: 'json' };
+import goldenvalley from '/data/polygons/goldenvalley.json' assert { type: 'json' };
+import bisley from '/data/polygons/bisley.json' assert { type: 'json' };
+import multi_polygon from '../data/polygons/multipolygon.js';
+import test from '../data/polygons/test.js';
+
 import routes from '/data/routes.json' assert { type: 'json' };
 import studios from '/data/studios.json' assert { type: 'json' };
+import studio_markers from '/data/studio-markers.js';
+import styles from '/scripts/styles.js'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF0dGhpYXN3ZXN0b24iLCJhIjoiY2xlNHIya255MDJqaTNwbXY5NjUzdWgzYSJ9.af8OJ3gOuIiOvKkYllihGQ';
 
@@ -27,29 +40,30 @@ const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/matthiasweston/clhuty4u3020p01r0f1wb6lwo',
     center: [-2.181235, 51.736333],
-    zoom: 11.15
+    zoom: 11.5
 });
 
-/*
-// Send the request to Mapbox
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    // Extract the route geometry and instructions
-    const routeGeometry = data.routes[0].geometry;
+// Initial markers for all studios
+studio_markers.forEach(({studio, color, lngLat}) => {
+    const popup = new mapboxgl.Popup({ offset: 25,}).setHTML(studio) // change to info setHTML
 
-    // Print the route geometry and instructions
-    console.log(`Route Geometry: ${routeGeometry}`);
-    console.log("Route Instructions:");
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
-*/
+    new mapboxgl.Marker({
+        color: color,
+        scale: 0.8
+    })
+    .setLngLat(lngLat)
+    .setPopup(popup)
+    .addTo(map)
+
+   /* marker.addEventListener('mouseenter', function() {
+        popup.togglePopup()
+    }) */
+})
 
 map.on('load', () => {
     map.addControl(new mapboxgl.NavigationControl(), 'top-left');
-
+    //map.setPaintProperty('routes', 'line-width', 3);
+   
     /**
      * @note Directions API : request up to 25 waypoints only
      */
@@ -60,7 +74,7 @@ map.on('load', () => {
                 // enableHighAccuracy: true
             },
             fitBoundsOptions: {
-                maxZoom: 12 // edit this
+                maxZoom: 13 // edit this
             },
             trackUserLocation: true,
             showUserHeading: true,
@@ -72,25 +86,26 @@ map.on('load', () => {
         new MapboxDirections({
             accessToken: mapboxgl.accessToken,
             unit: 'metric',
-           
+           //steps: true,
+           //banner_instructions: true,
             // coordinates: 
-            profile: 'mapbox/driving',
+            profile: 'mapbox/cycling',
             alternatives: false,
             controls: {
                 instructions: false,
                 inputs: false,
             },
-            interactive: false
+            interactive: false,
+            styles: styles
         });
 
     map.addControl(directions,
         'top-left'
     );
 
-    map.addSource('studios', {
-        'type': 'geojson',
-        'data': studios
-    });
+    
+/* MAIN POLYGON
+
     map.addSource('polygon', {
         'type': 'geojson',
         'data': polygon
@@ -115,6 +130,234 @@ map.on('load', () => {
             'line-width': 1
         }
     });
+
+    */
+
+
+
+
+   /* map.addSource('test', {
+        'type': 'geojson',
+        'data': test
+    });
+    map.addLayer({
+        'id': 'test',
+        'type': 'fill',
+        'source': 'test',
+        'layout': {},
+        'paint': {
+            'fill-color': '#f84d4d',
+            'fill-opacity': polygon_opacity
+        }
+    });
+     map.addLayer({
+        'id': 'test-outline',
+        'type': 'line',
+        'source': 'test',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    });*/
+   /*
+
+    map.addSource('towncentre', {
+        'type': 'geojson',
+        'data': towncentre
+    });
+    map.addLayer({
+        'id': 'towncentre',
+        'type': 'fill',
+        'source': 'towncentre',
+        'layout': {},
+        'paint': {
+            'fill-color': '#f84d4d',
+            'fill-opacity': polygon_opacity
+        }
+    });
+    map.addLayer({
+        'id': 'town-outline',
+        'type': 'line',
+        'source': 'towncentre',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    }); /*
+
+    map.addSource('nailsworth', {
+        'type': 'geojson',
+        'data': nailsworth
+    });
+    map.addLayer({
+        'id': 'nailsworth',
+        'type': 'fill',
+        'source': 'nailsworth',
+        'layout': {},
+        'paint': {
+            'fill-color': '#4264fb',
+            'fill-opacity': polygon_opacity
+        }
+    });
+    map.addLayer({
+        'id': 'nailsworth-outline',
+        'type': 'line',
+        'source': 'nailsworth',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    });
+
+    map.addSource('vale', {
+        'type': 'geojson',
+        'data': vale
+    });
+    map.addLayer({
+        'id': 'vale',
+        'type': 'fill',
+        'source': 'vale',
+        'layout': {},
+        'paint': {
+            'fill-color': '#40546a',
+            'fill-opacity': polygon_opacity
+        }
+    });
+    map.addLayer({
+        'id': 'vale-outline',
+        'type': 'line',
+        'source': 'vale',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    });
+
+
+    // 4 SLAD POLY
+    map.addSource('slad', {
+        'type': 'geojson',
+        'data': slad
+    });
+    map.addLayer({
+        'id': 'slad',
+        'type': 'fill',
+        'source': 'slad',
+        'layout': {},
+        'paint': {
+            'fill-color': '#f79640',
+            'fill-opacity': 0.3
+        }
+    });
+    map.addLayer({
+        'id': 'slad-outline',
+        'type': 'line',
+        'source': 'slad',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    });
+
+    // 5 bisley
+    map.addSource('bisley', {
+        'type': 'geojson',
+        'data': bisley
+    });
+    map.addLayer({
+        'id': 'bisley',
+        'type': 'fill',
+        'source': 'bisley',
+        'layout': {},
+        'paint': {
+            'fill-color': '#7753eb',
+            'fill-opacity': 0.3
+        }
+    });
+    map.addLayer({
+        'id': 'bisley-outline',
+        'type': 'line',
+        'source': 'bisley',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    });
+
+    map.addSource('goldenvalley', {
+        'type': 'geojson',
+        'data': goldenvalley
+    });
+    map.addLayer({
+        'id': 'goldenvalley',
+        'type': 'fill',
+        'source': 'goldenvalley',
+        'layout': {},
+        'paint': {
+            'fill-color': '#207a4b',
+            'fill-opacity': 0.3
+        }
+    });
+    map.addLayer({
+        'id': 'golden-outline',
+        'type': 'line',
+        'source': 'goldenvalley',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    }); */
+
+    /* MULTIPOLYGON */
+
+   // const polygon_colors = ['#f84d4d', '#4264fb', '#40546a', '#f79640', '#7753eb', '#207a4b'];
+
+   const line_width = 2;
+   const polygon_opacity = 0.5;
+
+    map.addSource('multi_polygon', {
+        type: 'geojson',
+        data: multi_polygon
+      });
+
+      multi_polygon.features.forEach((feature) => {
+        const layerID = `polygon-${feature.properties.color}`
+        const polygon_opacity = 0.5;
+        map.addLayer({
+          id: layerID,
+          type: 'fill',
+          source: 'multi_polygon',
+          paint: {
+            'fill-color': ['get', 'color'],
+            'fill-opacity': 0.1
+          },
+          layout: {}
+        });
+      });
+
+      map.addLayer({
+        'id': 'poly-outline',
+        'type': 'line',
+        'source': 'multi_polygon',
+        'layout': {},
+        'paint': {
+            'line-color': '#000',
+            'line-width': line_width
+        }
+    });
+
+
+    /**
+     * @Routes (5 Valleys)
+     */
+/*
     map.addSource('routes', {
         'type': 'geojson',
         'data': routes
@@ -127,6 +370,8 @@ map.on('load', () => {
             'icon-image': ['get', 'icon'],
             'icon-size': 1.1,
             'text-field': ['get', 'title'],
+            'icon-allow-overlap': true,
+            'text-allow-overlap': true,
             'text-font': [
                 'Open Sans Semibold',
                 'Arial Unicode MS Bold'
@@ -139,7 +384,17 @@ map.on('load', () => {
             'text-color': ['get', 'color']
         }
     });
-   
+    */
+
+    /**
+     * @studios
+     */
+
+    map.addSource('studios', {
+        'type': 'geojson',
+        'data': studios
+    });
+
     for (const feature of studios.features) {
         const symbol = feature.properties.icon;
         const waypoint = feature.geometry.coordinates;
@@ -152,8 +407,9 @@ map.on('load', () => {
                 'source': 'studios',
                 'layout': {
                     'icon-image': `${symbol}`,
-                    'icon-size': 1,
-                    'icon-allow-overlap': true,
+                    'icon-size': 0.9,
+                   // 'icon-allow-overlap': true,
+                   // 'text-allow-overlap': true,
                     'text-field': ['get', 'title'],
                     'text-font': [
                         'Open Sans Semibold',
@@ -164,18 +420,20 @@ map.on('load', () => {
                     'text-size': 18
                 },
                 'paint': {
-                    'text-color': ['get', 'color']
+                    'text-color': ['get', 'color'],
+                    'line-color': '#ffffff',
+                    'line-width': 6
                 },
                'filter': ['==', 'icon', symbol]
-            });
 
+            });
             map.setLayoutProperty(layerID, 'visibility', 'none');
 
             const input = document.createElement('input');
             input.type = 'checkbox';
             input.id = layerID;
             input.checked = false;
-            filterGroup.appendChild(input);
+            filterGroup.appendChild(input);  // class in map project html
 
             const label = document.createElement('label');
             label.setAttribute('for', layerID);
@@ -188,14 +446,45 @@ map.on('load', () => {
                 directions.setOrigin([userLongitude, userLatitude]);
                 directions.setDestination([userLongitude, userLatitude]);
             })
-            input.addEventListener('change', (e) => {
+
+            /**
+             * Route Colour
+             */
+
+             // Listen to the `route` event
+             directions.on('route', function() {
+                var routeColor = '#f84d4d';
+                var routeColor = '#ff6868';
+                var routeOutlineColor = '#111';
+                // Access the route layer and update its paint properties
+                map.setPaintProperty('directions-route-line', 'line-color', routeColor, 'line-width', 8);
+                map.setPaintProperty('directions-route-line-alt', 'line-color', routeOutlineColor);                
+            });
+            
+
+            // If want to add waypoint but don't remove on uncheck
+           /* input.addEventListener('change', (e) => {
                 map.setLayoutProperty(
                     layerID,
                     'visibility',
-                    e.target.checked ? 'visible' : 'none',
+                    // on change check if checked and return visible or none
+                    e.target.checked ? 'visible' : 'none'
                 );
-                directions.addWaypoint(0, waypoint);
+                directions.addWaypoint(0, waypoint)
             });
+            */
+           
+            // Add / remove waypoints
+           input.addEventListener('change', (e) => {
+               if (e.target.checked) {
+                   map.setLayoutProperty(layerID, 'visibility', 'visible');
+                   directions.addWaypoint(0, waypoint);
+               } else {
+                   map.setLayoutProperty(layerID, 'visibility', 'none')
+                   directions.removeWaypoint(0);
+               }
+           })
         }
     }
 });
+
