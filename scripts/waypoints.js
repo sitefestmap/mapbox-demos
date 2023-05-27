daad38286a92cfa1eae0f406f89182cffb1a895d
mattweston <68040892+mattweston@users.noreply.github.com>
2023-05-27 19:26:43 +0100

    tidy coords

--- scripts/waypoints.js
+++
@@ -1,7 +0,0 @@
-const waypoints = [
-    [-2.2167788, 51.7445037], // John St
-    [-2.21798684, 51.74506395], // Weven
-    [-2.2170453, 51.7451129] // Cacao Circl
-];
-
-const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${waypoints.join(';')}?access_token=${mapboxgl.accessToken}`;
